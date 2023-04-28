f52d44a577f68c6f64ec83cda77f438d29018889
Flor <flor.rodriguez@tecsup.edu.pe>
2023-04-27 22:06:24 -0500

    Cambios actualizados

--- src/main/java/com/tecsup/lab03/controller/StudentController.java
+++ src/main/java/com/tecsup/lab03/controller/StudentController.java
@@ -1,5 +1,29 @@
 package com.tecsup.lab03.controller;
-
+import java.util.ArrayList;
+import java.util.List;
+import com.tecsup.lab03.model.Student;
 public class StudentController {
+	
+	private List<Student> students;
 
+    public StudentController() {
+        this.students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void removeStudent(Student student) {
+        students.remove(student);
+    }
+
+    public void updateStudent(Student student) {
+        int index = students.indexOf(student);
+        students.set(index, student);
+    }
+
+    public List<Student> getAllStudents() {
+        return students;
+    }
 }
